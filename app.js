d6944dbf229cf6ffd634220893eeb0ee603293d7
roushan-jha <jharoushan20@gmail.com>
2024-09-26 22:06:25 +0530

    fixed internal server error

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const http = require('http');
 const path = require('path');
 
 const app = express();
+const cors = require('cors');
+const corsConfig = {
+    origin: "*",
+    credential: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+}
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig));
 
 const socketio = require("socket.io");
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
